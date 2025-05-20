4a3c7e336fb616e1e558d927913c08f710ceced7
sof202 <s.o.fletcher@exeter.ac.uk>
2025-05-20 13:06:50 +0100

    feat: add installation script
    The script is pretty comprehensive and should cover most shells. Poetry
    and python are not really a great fit for making a CLI tool. This is the
    best I could come up with (a custom shell script writer that runs squire
    for you). Might be some problems with some shells and still requires one
    to have bash installed

---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+set -e
+
+# -----------------
+# Install directory
+# -----------------
+
+DEFAULT_INSTALL_DIR="${HOME}/.local/bin"
+install_dir="${1:-$DEFAULT_INSTALL_DIR}"
+mkdir -p "${install_dir}"
+
+# -------------------
+# Poetry Installation
+# -------------------
+
+cd "$(dirname "$0")" || exit 1
+poetry install
+
+# ---------------
+# Create launcher
+# ---------------
+
+cat > "${install_dir}/squire" << EOF
+#!${SHELL}
+. "$(poetry env info --path)/bin/activate"
+exec squire "\$@"
+EOF
+chmod +x "${install_dir}/squire"
+
+# ------------------
+# PATH message setup
+# ------------------
+
+get_shell_rc() {
+case "$(basename "${SHELL:-}")" in
+    bash)
+        if [[ -f "${HOME}/.bash_profile" ]]; then 
+            echo "${HOME}/.bash_profile"
+        else 
+            echo "${HOME}/.bashrc"
+        fi  ;;
+    zsh)
+        if [[ -f "${HOME}/.zprofile" ]]; then 
+            echo "${HOME}/.zprofile"
+        else 
+            echo "${HOME}/.zshrc"
+        fi  ;;
+    fish)
+        echo "${HOME}/.config/fish/config.fish" ;;
+    *)
+        echo "${HOME}/.profile" ;;
+esac
+}
+
+rc_file=$(get_shell_rc)
+
+path_instructions="echo 'export PATH=\"${install_dir}:\$PATH\"' >> \"${rc_file}\""
+if [[ "$(basename "${SHELL:-}")" == "fish" ]]; then
+    path_instructions="echo 'set -gx PATH \"${install_dir}\" \$PATH' >> \"${rc_file}\""
+fi
+
+# ------------
+# PATH message
+# ------------
+
+GREEN='[0;32m'
+YELLOW='[0;33m'
+NO_COLOUR='[0m'
+
+if echo "${PATH}" | grep -q "${install_dir}"; then
+cat << EOF
+${GREEN}Installed squire successfully at ${install_dir}. 
+EOF
+else
+cat << EOF
+${GREEN}Installed squire successfully at ${install_dir}. 
+${YELLOW}
+WARNING: ${install_dir} is not on your PATH please add it by running:
+    ${NO_COLOUR}${path_instructions}
+${YELLOW}
+Then restart your shell or run:
+    ${NO_COLOUR}source "${rc_file}"
+    (or . "${rc_file}" for POSIX sh)
+EOF
+fi
