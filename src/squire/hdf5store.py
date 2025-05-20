38dc67b78bf4e03736561b73e86fe619e0d2c82d
sof202 <s.o.fletcher@exeter.ac.uk>
2025-05-20 16:56:52 +0100

    feat: add hdf5 store functions for reading in files
    Basic plan is to create a hdf5 file for storing all of the data nicely
    (less memory usage) and then this can be used to generate the reference
    matrix (concat) and also can be chunked to do the statistical analysis
    in parallel.

---
+++ src/squire/hdf5store.py
@@ -0,0 +1,62 @@
+import os
+import pandas as pd
+
+
+def get_file_basename(file_path):
+    return os.path.splitext(os.path.basename(file_path))[0]
+
+
+def add_file_to_hdf_store(file_path, hdf_path, chunk_size=100_000):
+    with pd.HDFStore(hdf_path, mode="a") as store:
+        columns_to_keep = [0, 1, 2, 3, 4, 11]
+        column_names = ["chr", "start", "end", "name", "read_depth", "modifications"]
+        column_dtypes = {
+            "chr": str,
+            "start": int,
+            "end": int,
+            "name": str,
+            "read_depth": int,
+            "modifications": int,
+        }
+        basename = get_file_basename(file_path)
+        for chunk in pd.read_csv(
+            file_path,
+            sep=r"\s+",  # bedmethyl has mix of tabs and spaces for separators
+            header=None,
+            usecols=columns_to_keep,  # type: ignore[arg-type]
+            names=column_names,
+            dtype=column_dtypes,  # type: ignore[arg-type]
+            chunksize=chunk_size,
+        ):
+            chunk["fraction"] = chunk["modifications"] / chunk["read_depth"]
+            store.append(f"data/{basename}", chunk)
+
+
+def generate_coordinate_index(hdf_path, chunk_size=100_000):
+    with pd.HDFStore(hdf_path, mode="a") as store:
+        all_coordinates = []
+        data_paths = [key for key in store.keys() if key.startswith("/data/")]
+        for path in data_paths:
+            for chunk in store.select(
+                path, columns=["chr", "start", "end", "name"], chunksize=chunk_size
+            ):
+                all_coordinates.append(chunk)
+
+        coordinates = (
+            pd.concat(all_coordinates, ignore_index=True)
+            .drop_duplicates()
+            .sort_values(by=["chr", "start", "end", "name"])
+            .reset_index(drop=True)
+            .astype(
+                {
+                    "chr": "category",
+                    "start": "uint32",
+                    "end": "uint32",
+                    "name": "category",
+                }
+            )
+        )
+        store.put("coordinates", coordinates, format="table", data_columns=True)
+        store.create_table_index(
+            "coordinates", columns=["chr", "start", "end"], kind="full"
+        )
