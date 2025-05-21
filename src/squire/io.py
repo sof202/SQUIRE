74c63e0cbfae38504d49cbfb7892cff6dad644c4
sof202 <s.o.fletcher@exeter.ac.uk>
2025-05-21 16:24:24 +0100

    feat: raise exceptions if input files are not viable

--- src/squire/io.py
+++ src/squire/io.py
@@ -27,6 +27,67 @@ def make_viable_path(path: Path, exists_ok=True):
         raise PermissionError(f"You do not have permissions to create {path}.")
 
 
+def is_viable_bedmethyl(bedmethyl_path: Path, number_of_rows_to_check=5):
+    if not bedmethyl_path.exists():
+        return False
+    if not bedmethyl_path.is_file():
+        return False
+    if bedmethyl_path.stat().st_size == 0:
+        return False
+
+    expected_dtypes = [
+        "str",  # chrom
+        "int64",  # start position
+        "int64",  # end position
+        "str",  # modified base code
+        "int64",  # score
+        "str",  # strand
+        "int64",  # start position (compat)
+        "int64",  # end position (compat)
+        "str",  # color
+        "int64",  # Nvalid_cov
+        "float64",  # fraction modified
+        "int64",  # Nmod
+        "int64",  # Ncanonical
+        "int64",  # Nother_mod
+        "int64",  # Ndelete
+        "int64",  # Nfail
+        "int64",  # Ndiff
+        "int64",  # Nnocall
+    ]
+
+    try:
+        top_rows = pd.read_csv(
+            bedmethyl_path,
+            sep=r"\s+",
+            header=None,
+            nrows=number_of_rows_to_check,
+            dtype={9: "str"},  # force color column to be string
+        )
+
+        if top_rows.shape[1] != 18:
+            return False
+
+        for i, (expected, actual) in enumerate(zip(expected_dtypes, top_rows.dtypes)):
+            if str(actual) != expected:
+                return False
+
+        return True
+
+    except Exception:
+        return False
+
+
+def is_viable_hdf5(hdf_path: Path):
+    if not hdf_path.exists() or not hdf_path.is_file():
+        return False
+    try:
+        with pd.HDFStore(hdf_path, mode="r"):
+            return True
+    except (OSError, IOError):
+        return False
+
+
 def export_reference_matrix(hdf_path, out_file_path):
     with pd.HDFStore(hdf_path, mode="r") as store:
         merged = store["merged_data"]
