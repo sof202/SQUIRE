fc382ce5f0e755e9dad5fa1dcb293551156c49d5
sof202 <s.o.fletcher@exeter.ac.uk>
2025-05-22 16:27:07 +0100

    feat: update PBS script with an example of automating threshold analysis

--- scripts/pbs_example.sh
+++ scripts/pbs_example.sh
@@ -11,5 +11,18 @@
 squire create -d squire.h5 -b neuron.bed,olig.bed,glial.bed
 squire add -d squire.h5 -b astrocyte.bed
 squire reference -d squire.h5 reference_matrix.bed
-squire cpglist -d squire.h5 -t 1e-15 cpg_list.bed
+
+# Get threshold where number of passing CpGs is as close as possible to 
+# some value
+cpgs_to_keep=10000
+threshold_list="1e-5,1e-10,1e-15,1e-20"
+threshold_to_use=$(\
+    squire report -d squire.h5 -m -t "${threshold_list}" | \
+    awk \
+        -v max_value="${cpgs_to_keep}"\
+        -F":" \
+        '{if ($2<max_value) {print $1; exit}}'
+)
+
+squire cpglist -d squire.h5 -t "${threshold_to_use}" cpg_list.bed
 
